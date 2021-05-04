cae7a532c786ab376be281dc976b91e97a83f34a
Pierre Penhard <p.penhard@gmail.com>
2021-05-04 22:29:44 +0200

    Added login and signup btns

--- client/pages/components/Navbar.js
+++ client/pages/components/Navbar.js
@@ -6,16 +6,17 @@ export default function Navbar() {
      <nav className='bg-gray-800'>
         <div className='flex items-center justify-between h-16 mx-8 -inset-x-0'>
           <div>
-            <h1 className='font-semibold text-white'>Trackt</h1>
+            <h1 className='font-extrabold text-white'>Trackt</h1>
           </div>
           <div className='flex w-1/3 justify-evenly'>
-            <a href="#" className='font-light text-gray-100'>Home</a>
-            <a href="#" className='font-light text-gray-100'>Movies</a>
-            <a href="#" className='font-light text-gray-100'>TV Shows</a>
-            <a href="#" className='font-light text-gray-100'>Upcoming</a>
+            <a href="#" className='font-light text-white opacity-60 hover:opacity-100'>Home</a>
+            <a href="#" className='font-light text-white opacity-60 hover:opacity-100'>Movies</a>
+            <a href="#" className='font-light text-white opacity-60 hover:opacity-100'>TV Shows</a>
+            <a href="#" className='font-light text-white opacity-60 hover:opacity-100'>Upcoming</a>
           </div>
-          <div>
-            <h1 className='font-semibold text-white'>Se connecter</h1>
+          <div className='flex space-x-6'>
+            <a className='flex items-center font-medium text-white'>Login</a>
+            <a className='flex items-center h-10 px-6 py-4 font-medium text-white bg-indigo-600 rounded-full'>Signup</a>
           </div>
         </div>
      </nav>
