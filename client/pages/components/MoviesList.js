d1c6b1938c5f1a3ac7e228e97f46ec1d28cc531a
Pierre Penhard <p.penhard@gmail.com>
2021-05-05 23:09:47 +0200

    Added some kind of responsivity

--- client/pages/components/MoviesList.js
+++ client/pages/components/MoviesList.js
@@ -3,7 +3,7 @@ import Movie from './Movie';
 
 function MoviesList({ movies }) {
   return (
-    <div className="grid grid-cols-8 gap-x-1 gap-y-4">
+    <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-8 gap-x-1 gap-y-4">
       {movies.map((movie) => (
         <div className="flex justify-center">
           <Movie movie={movie} />
