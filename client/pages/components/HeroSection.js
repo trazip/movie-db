10bb0cdec88e6e7e6dba8ad715624398aa9db522
Pierre Penhard <p.penhard@gmail.com>
2021-05-05 17:41:42 +0200

    Added Wes bos config for ESlint

--- client/pages/components/HeroSection.js
+++ client/pages/components/HeroSection.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 export default function HeroSection() {
   return (
     <>
-     <div className="relative flex justify-center mt-8">
-      <Image
+      <div className="relative flex justify-center mt-8">
+        <Image
           src="/got.jpeg"
           alt="game of thrones poster"
           width={1300}
           height={400}
           objectFit="cover"
           quality={100}
-          className='rounded-lg shadow'
+          className="rounded-lg shadow"
         />
-     </div>
+      </div>
     </>
-  )
+  );
 }
--- client/pages/components/Navbar.js
+++ client/pages/components/Navbar.js
@@ -1,25 +1,47 @@
-import React from 'react'
+import React from 'react';
 
 export default function Navbar() {
   return (
     <>
-     <nav className='bg-gray-800'>
-        <div className='flex items-center justify-between h-16 mx-8 -inset-x-0'>
+      <nav className="bg-gray-800">
+        <div className="flex items-center justify-between h-16 mx-8 -inset-x-0">
           <div>
-            <h1 className='font-extrabold text-white'>Trackt</h1>
+            <h1 className="font-extrabold text-white">Trackt</h1>
           </div>
-          <div className='flex w-1/3 justify-evenly'>
-            <a href="#" className='font-light text-white opacity-60 hover:opacity-100'>Home</a>
-            <a href="#" className='font-light text-white opacity-60 hover:opacity-100'>Movies</a>
-            <a href="#" className='font-light text-white opacity-60 hover:opacity-100'>TV Shows</a>
-            <a href="#" className='font-light text-white opacity-60 hover:opacity-100'>Upcoming</a>
+          <div className="flex w-1/3 justify-evenly">
+            <a
+              href="#"
+              className="font-light text-white opacity-60 hover:opacity-100"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="font-light text-white opacity-60 hover:opacity-100"
+            >
+              Movies
+            </a>
+            <a
+              href="#"
+              className="font-light text-white opacity-60 hover:opacity-100"
+            >
+              TV Shows
+            </a>
+            <a
+              href="#"
+              className="font-light text-white opacity-60 hover:opacity-100"
+            >
+              Upcoming
+            </a>
           </div>
-          <div className='flex space-x-6'>
-            <a className='flex items-center font-medium text-white'>Login</a>
-            <a className='flex items-center h-10 px-6 py-4 font-medium text-white bg-indigo-600 rounded-full'>Signup</a>
+          <div className="flex space-x-6">
+            <a className="flex items-center font-medium text-white">Login</a>
+            <a className="flex items-center h-10 px-6 py-4 font-medium text-white bg-indigo-600 rounded-full">
+              Signup
+            </a>
           </div>
         </div>
-     </nav>
+      </nav>
     </>
-  )
+  );
 }
